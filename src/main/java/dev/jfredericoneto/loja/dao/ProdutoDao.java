a7b096d0539aed846c5343b967310ecd5918359f
Neto <jfredericon@gmail.com>
2024-01-27 17:42:58 -0300

    Adicionado métodos de busca ao ProdutoDao

--- src/main/java/dev/jfredericoneto/loja/dao/ProdutoDao.java
+++ src/main/java/dev/jfredericoneto/loja/dao/ProdutoDao.java
@@ -1,5 +1,8 @@
 package dev.jfredericoneto.loja.dao;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import dev.jfredericoneto.loja.modelo.Produto;
 import jakarta.persistence.EntityManager;
 
@@ -22,6 +25,36 @@ public class ProdutoDao {
     public void remover(Produto produto) {
         produto = em.merge(produto);
         this.em.remove(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        return em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
     }
 
 }
--- src/main/java/dev/jfredericoneto/loja/testes/CadastroDeProduto.java
+++ src/main/java/dev/jfredericoneto/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package dev.jfredericoneto.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import dev.jfredericoneto.loja.dao.CategoriaDao;
 import dev.jfredericoneto.loja.dao.ProdutoDao;
@@ -13,6 +14,22 @@ public class CadastroDeProduto {
 
     public static void main(String[] args) {
 
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto produto1 = produtoDao.buscarPorId(1l);
+        System.out.println(produto1.getPreco());
+
+        List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(produto -> System.out.println(produto.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Samsung Galaxy S21 FE");
+        System.out.println("Preco do Produto: " + precoDoProduto);
+
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
 
         Produto celular = new Produto("Samsung Galaxy S21 FE", "Celular intermediário", new BigDecimal(1500),
